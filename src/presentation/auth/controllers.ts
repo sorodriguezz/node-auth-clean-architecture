02e5e12a0089c3fc29f6856065c0c1d5d6db24b2
soRodriguezz <s.rodriguezzapata01@gmail.com>
2025-04-04 14:56:17 -0300

    add validate token

--- src/presentation/auth/controllers.ts
+++ src/presentation/auth/controllers.ts
@@ -38,8 +38,7 @@ export class AuthController {
     getUsers = (req: Request, res: Response) => {
         UserModel.find()
             .then(users => res.json({
-                users,
-                token: req.body.token,
+                user: req.body.user,
             }))
             .catch(error => res.status(500).json({error: 'Internal Server Error'}));
     }
--- src/presentation/middlewares/auth.middleware.ts
+++ src/presentation/middlewares/auth.middleware.ts
@@ -1,18 +1,24 @@
 import {NextFunction, Request, Response} from "express";
+import {JwtAdapter} from "../../config";
+import {UserModel} from "../../data";
 
 export class AuthMiddleware {
-    static validateJWT = (req: Request, res: Response, next: NextFunction) => {
+    static validateJWT = async (req: Request, res: Response, next: NextFunction) => {
         const authorization = req.header('authorization') || '';
 
-        if (!authorization) res.status(401).send({ error: 'Not authorized'});
-        if (!authorization.startsWith('Bearer ')) res.status(401).send({ error: 'Invalid token'});
+        if (!authorization) res.status(401).send({error: 'Not authorized'});
+        if (!authorization.startsWith('Bearer ')) res.status(401).send({error: 'Invalid token'});
 
         const token = authorization.split(' ').at(1) || '';
 
         try {
-            // const payload
+            const payload = await JwtAdapter.validateToken<{ id: string }>(token);
+            if (!payload) res.status(401).send({error: 'Invalid token'});
 
-            req.body.token = token;
+            const user = await UserModel.findById(payload!.id);
+            if (!user) res.status(401).send({error: 'Invalid token'});
+
+            req.body = req.body || {user};
 
             next();
         } catch (error) {
